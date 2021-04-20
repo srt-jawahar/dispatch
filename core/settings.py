091d431e4041657303cf86488d650fb3b477cc15
monish n <knockme96@gmail.com>
2021-04-20 19:50:38 +0530

    suffering is suffering

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -162,7 +162,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
