c0057afebc149146e47bae02e9188a5bb7d2cdd1
monish n <knockme96@gmail.com>
2021-04-20 21:30:29 +0530

    end of suffering

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -181,4 +181,4 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
