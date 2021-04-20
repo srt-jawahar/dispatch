2c647b3f981d686ba0feaaaf3890893133eb7479
monish n <knockme96@gmail.com>
2021-04-20 15:51:21 +0530

    Its fine to struggle

--- core/settings.py
+++ core/settings.py
@@ -155,6 +155,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
