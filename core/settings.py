22587e9700be4d9b4412957c3e9da5b752f64156
monish n <knockme96@gmail.com>
2021-04-20 15:42:14 +0530

    Its fine to struggle

--- core/settings.py
+++ core/settings.py
@@ -152,8 +152,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
+# STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
