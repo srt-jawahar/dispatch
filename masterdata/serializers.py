072c18aa70501d9ffdd9723d98131a8c7d2b310e
Muthuganesh.B <muthuganesh200591@gmail.com>
2021-05-17 17:14:45 +0530

    Plant master updated and Customer master added

--- masterdata/serializers.py
+++ masterdata/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Plants
+from .models import Plants, Customers
 from django.contrib.auth.models import User
 
 class PlantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plants
+        fields = '__all__'
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customers
         fields = '__all__'
--- masterdata/views.py
+++ masterdata/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import mixins, status, permissions
 from rest_framework import generics
-from .serializers import PlantSerializer
-from .models import Plants
+from .serializers import PlantSerializer, CustomerSerializer
+from .models import Plants, Customers
 from rest_framework.response import Response
 
 class PlantFromSAPView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,
@@ -10,13 +10,6 @@ class PlantFromSAPView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Cr
     serializer_class = PlantSerializer
     queryset = Plants.objects.all()
     lookup_field = 'PLANT'
-
-    def get(self, request, PLANT=None):
-
-        if PLANT:
-            return self.retrieve(request)
-        else:
-            return self.list(request)
 
     def post(self, request, PLANT=None):
         serializer = self.get_serializer(data=request.data, many=True)
@@ -30,3 +23,53 @@ class PlantFromSAPView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Cr
 
     def delete(self, request, PLANT=None):
         return self.destroy(request, PLANT)
+
+
+class PlantListView(generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    serializer_class = PlantSerializer
+    queryset = Plants.objects.all()
+    lookup_field = 'PLANT'
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, PLANT=None):
+
+        if PLANT:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+
+
+class CustomerFromSAPView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,
+                      mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = CustomerSerializer
+    queryset = Customers.objects.all()
+    lookup_field = 'CUST_ID'
+
+    def post(self, request, CUST_ID=None):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def put(self, request, CUST_ID=None):
+        return self.update(request, CUST_ID)
+
+    def delete(self, request, CUST_ID=None):
+        return self.destroy(request, CUST_ID)
+
+
+
+class CustomerListView(generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    serializer_class = CustomerSerializer
+    queryset = Customers.objects.all()
+    lookup_field = 'CUST_ID'
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, CUST_ID=None):
+
+        if CUST_ID:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
