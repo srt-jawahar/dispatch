9d81eec659cc4a3faeb3b6094f95803f3574ca86
Muthuganesh.B <muthuganesh200591@gmail.com>
2021-05-31 13:54:08 +0530

    REST APi to Assign the truck to the freight order

--- freight_order/views.py
+++ freight_order/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Max, Min
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import mixins, status, permissions
 from rest_framework import generics
-from .serializers import FreightOrdersSerializer
+from .serializers import FreightOrdersSerializer, FreightTruckAssignSerializer
 from .models import FreightOrders
 from rest_framework.response import Response
 from truckmanagement.models import TruckAvailability, TruckDetails
@@ -75,6 +76,9 @@ class FreightView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Retriev
             avail_truck_ids = TruckAvailability.objects.filter(source_location=freight_order.from_location,
                                                                destination=freight_order.destination,
                                                                no_of_trucks__gte=1).values('truck_type_id')
+            if not avail_truck_ids:
+                return Response({"message": "No available trucks"}, status=status.HTTP_400_BAD_REQUEST)
+
             min_truck_value = TruckDetails.objects.filter(id__in=avail_truck_ids,
                                                           truck_max_capacity__gte=freight_order.total_weight).aggregate(
                 truck_max_capacity=Min('truck_max_capacity'))
@@ -104,6 +108,33 @@ class FreightView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Retriev
                         freight_order.suggested_truck_type = final_truck.truck_type
                         freight_order.no_of_trucks = truck_count
 
+            freight_order.created_by = request.user.username
+            freight_order.updated_by = request.user.username
             freight_order.save()
 
         return Response(status=status.HTTP_201_CREATED)
+# Ending of freight order creation
+
+
+# Starting of truck assignment to the freight orders
+class FreightTruckAssignView(generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    serializer_class = FreightTruckAssignSerializer
+    queryset = FreightOrders.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def put(self, request):
+        reqdata = request.data
+        serializer = self.get_serializer(data=reqdata, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        for freight_data in reqdata:
+            freight_order_no = freight_data.get('freight_order_no', None)
+            freight_order = FreightOrders.objects.filter(freight_order_no=freight_order_no)
+            for order in freight_order:
+                order.suggested_truck_type = freight_data.get('suggested_truck_type', None)
+                order.no_of_trucks = freight_data.get('no_of_trucks', None)
+                order.truck_status = 'Confirmed'
+                order.save()
+
+                return Response(status=status.HTTP_200_OK)
+# Ending of truck assignment to the freight orders
