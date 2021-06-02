c31640fa83e16c25d7540abc29faad29b3c2a7db
Muthuganesh.B <muthuganesh200591@gmail.com>
2021-06-02 20:21:43 +0530

    Modified the freight related APIs , models and serializers based on functional input

--- freight_order/serializers.py
+++ freight_order/serializers.py
@@ -1,18 +1,38 @@
 from rest_framework import serializers
-from .models import FreightOrders
+from .models import FreightOrders, FreightTruckAssignments
 
 
 class FreightOrdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = FreightOrders
         fields = '__all__'
-        read_only_fields = ('created_by', 'updated_by', 'freight_order_no', 'from_location', 'suggested_truck_type',
-                              'no_of_trucks', 'delivery_id', 'transportor_name')
+        read_only_fields = ('created_by', 'updated_by', 'freight_order_no', 'from_location',
+                            'delivery_id', 'transportor_name')
 
 
 class FreightTruckAssignSerializer(serializers.ModelSerializer):
     class Meta:
         model = FreightOrders
         fields = '__all__'
-        read_only_fields = ('delivery_no', 'created_by', 'updated_by', 'freight_order_no', 'from_location', 'delivery_id',
+        read_only_fields = ('delivery_no', 'created_by', 'updated_by',  'from_location', 'delivery_id',
                             'total_volume', 'total_weight', 'destination')
+
+
+class FreightTruckConfirmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FreightTruckAssignments
+        fields = '__all__'
+        read_only_fields = ('freight_order', 'created_by', 'updated_by', 'transportor_name')
+
+
+class FreightTruckAssignmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FreightTruckAssignments
+        fields = '__all__'
+
+
+class FreightOrdersGetSerializer(serializers.ModelSerializer):
+    freighttruckassignments_set = FreightTruckAssignmentsSerializer(many=True)
+    class Meta:
+        model = FreightOrders
+        fields = '__all__'
--- freight_order/urls.py
+++ freight_order/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import FreightView, FreightTruckAssignView
+from .views import FreightView, FreightTruckAssignView, FreightTruckConfirmView, GetAllFreightView
 
 
 urlpatterns = [
    # freight urls
-   path('freightOrder/', FreightView.as_view()),
+   path('freightOrder/', FreightView.as_view()),  # to create the freight order
    path('freightTruckAssign/', FreightTruckAssignView.as_view()),
+   path('freightTruckConfirm/', FreightTruckConfirmView.as_view()),  # to confirm the truck type
+   path('getFreightList/', GetAllFreightView.as_view()),  # to get list of freight orders
 ]
