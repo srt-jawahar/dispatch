03eb5eaa683ae511ef34c7a9b4272a4f89220cfa
Muthuganesh.B <muthuganesh200591@gmail.com>
2021-06-05 14:40:06 +0530

    Add the scheduler to reset the truck availability

---
+++ freight_order/task.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+import django
+django.setup()
+from truckmanagement.models import TruckAvailability
+
+
+def reset_no_of_trucks():
+    print('Background printing....')
+    truck_availabilities = TruckAvailability.objects.all()
+    for truck in truck_availabilities:
+        truck.no_of_trucks = 0
+        truck.no_of_trucks_reserved = 0
+        truck.save()
--- manage.py
+++ manage.py
@@ -9,6 +9,11 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
+        # call the background process to reset the avail trucks
+        from freight_order import job
+        from truckmanagement.views import TruckAvailability
+        job.start()
+        # End of call the background process to reset the avail trucks
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
